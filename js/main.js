47cda16d281b8ad9b9283b800cfd270fd77c1d36
Shaikh Ashiq Ali <shaikhashiqali12345@gmail.com>
2023-07-05 20:54:35 +0530

    1

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+    var current = location.pathname;
+    console.log(current);
+    $('#nav li a').each(function(){
+        var $this = $(this);
+        // if the current path is like this link, make it active
+        if($this.attr('href').indexOf(current) !== -1){
+            $this.addClass('active');
+        }
+    })
+})
---
+++ js/navbar.js
@@ -0,0 +1,13 @@
+document.write(`
+<section class="custom-navbar">
+  		<div id="nav">
+  			<ul>
+  				<li><a href="/index.html">Home</a></li>
+  				<li><a href="/about.html">About</a></li>
+  				<li><a href="/skills.html">Skills</a></li>
+                <li><a href="/project.html">Project</a></li>
+  				<li><a href="/contact.html">Contact Us</a></li>
+  			</ul>
+  		</div>
+  	</section>
+`);
